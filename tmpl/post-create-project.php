ed5d8392f23d33dc05dd18746175c6ce828c318a
vansari <kurt.junker@posteo.de>
2023-04-03 23:35:01 +0200

    fix scripts

--- tmpl/post-create-project.php
+++ tmpl/post-create-project.php
@@ -40,7 +40,7 @@ function replaceCustoms($target, array $replaces)
 
 foreach (glob('tmpl/files/*', GLOB_BRACE) as $distFile) {
     $target = false === strpos($distFile, '-dist')
-        ? pathinfo($distFile, PATHINFO_FILENAME)
+        ? pathinfo($distFile, PATHINFO_BASENAME)
         : substr(pathinfo($distFile, PATHINFO_FILENAME), -5) . pathinfo($distFile, PATHINFO_EXTENSION);
     echo "Copy $distFile to $target..." . PHP_EOL;
     copy($distFile, $target);
